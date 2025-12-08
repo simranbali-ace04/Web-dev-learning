b811263ef969872d4743cc7eb4114563a6ed2d9d
Ace <simranbali2006@gmail.com>
2025-12-09 01:52:56 +0530

    Updation/changes in variables

---
+++ 03_changes.js
@@ -0,0 +1,10 @@
+let gameName = "spiderman";
+gameName= "batman";
+console.log(gameName);
+//It will print the updated value
+
+const username= "ace_04";
+username= "Simran bali";
+console.log(username);
+//Using const keyword variable's value can't be changed
+
