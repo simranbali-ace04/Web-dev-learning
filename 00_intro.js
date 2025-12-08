e11fd85dac9ba04f28b16011ec8c67dd4d9077b3
Ace <simranbali2006@gmail.com>
2025-12-09 01:14:26 +0530

    JS basics and execution flow notes

---
+++ 00_intro.js
@@ -0,0 +1,11 @@
+console.log("Hello World!");
+/*behind the scenes:
+1.) before the code run-time
+source code - - - -> syntax tree
+           via parse (taking keywords and assembling it)
+2.) JIT compiler (just in time compiler)
+              next
+[Byte code(before 0 and 1 code conversion as this byte code fastens the binary code conversion)----> machine code ]
+              next
+ code execution
+*/
