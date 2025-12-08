7a1a09fc8bae73d2574d1b3aceacd98fbb0d2610
Ace <simranbali2006@gmail.com>
2025-12-09 01:45:30 +0530

    Datatypes and declaration

---
+++ 02_datatype.js
@@ -0,0 +1,17 @@
+console.log("Hello");
+console.log("Ace"); 
+// comment shortcut: Ctrl + /
+
+// var score= 97; old way of defining a variable
+
+let score= 97;
+let name= "Ace047";
+let isLoggedin= false;
+
+//object
+let teaType= ["lemon tea", "orange tea", "oolong tea"];
+let user= {firstname: "Abhishek", lastname: "Sharma"};
+
+let getScore = score; //borrowing value
+console.log(getScore);
+
