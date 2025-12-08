92ae560fbc30c547080b1b7e4e5404932529b4e7
Ace <simranbali2006@gmail.com>
2025-12-09 01:29:40 +0530

    Printing JS

---
+++ 01_printing.js
@@ -0,0 +1,6 @@
+console.log("Hello");
+console.log("Hello");
+process.stdout.write("Yooo!"); // node way of printing
+process.stdout.write("Yooo!");// con: doesn't print in a new line
+
+console.table({city: "Chandigarh"});
