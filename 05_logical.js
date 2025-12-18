65b4b368d6c7085a8827735ee643cd0a52ec4aa3
Ace <simranbali2006@gmail.com>
2025-12-18 14:00:18 +0530

    logical ops

---
+++ 05_logical.js
@@ -0,0 +1,21 @@
+// && - and
+// || - or
+// ! - not
+
+let isLoggedin= true;
+let isPaid= false;
+
+console.log(isLoggedin && isPaid);
+
+let isEmailuser= true;
+let isGoogleuser= false;
+
+console.log(isEmailuser || isGoogleuser);
+
+
+//assignment operators
+
+let num1= 10; //kind 1
+console.log(num1);
+num1+= 5; //num = num+5
+console.log(num1);
