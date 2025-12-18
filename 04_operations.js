c95bc56a3d6c43a2b6e385e8e653ed3132328795
Ace <simranbali2006@gmail.com>
2025-12-18 13:49:48 +0530

    operations intro

---
+++ 04_operations.js
@@ -0,0 +1,31 @@
+let score= 102;
+let bonus= 25;
+
+let totalscore= score + bonus;
+
+let addition= 4+5;
+let subtraction= 9-3;
+let mult= 3*5;
+let div= 8/2;
+let remainder= 9%2;
+let expo= 2**3;  //exponential operator : works well with not so big data
+
+let myScore= 100;
+myScore++;
+
+let credits= 56;
+credits--;
+
+console.log(myScore);
+console.log(credits);
+
+//Comparison operations
+
+let num1= 3; //assign operator
+let num2= 3; 
+let num3= 6;
+
+console.log(num1==num2); //equal operator
+console.log(num1==num3);
+console.log(num1!=num3); //not equal
+console.log(num1>num3); //works same as maths
