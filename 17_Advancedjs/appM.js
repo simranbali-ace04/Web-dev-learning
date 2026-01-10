dcd36fadc0e8116ebaa82b8abbea3c0cbc54b81c
Ace <simranbali2006@gmail.com>
2026-01-10 13:19:53 +0530

    ES6 Module export

---
+++ 17_Advancedjs/appM.js
@@ -0,0 +1,10 @@
+//Module import file
+
+//default import
+import multiply from "./mathOperationsM";
+
+//named import
+import {add, subract} from "./mathOperationsM" ;
+
+console.log(multiply(2,3));
+console.log(add(2,5));
